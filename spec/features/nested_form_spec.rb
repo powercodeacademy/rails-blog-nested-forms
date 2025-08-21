c74a6b96df308d34057c0cf832fdecfba08cf9da
Stephen McKeon <stephen.mckeon@powerhrg.com>
2025-08-21 11:01:29 -0400

    Create post in spec

--- spec/features/nested_form_spec.rb
+++ spec/features/nested_form_spec.rb
@@ -1,21 +1,23 @@
 require 'rails_helper'
 
 describe 'nested form for tag in post', :type => :feature do
+  let(:post1) { Post.new(name: 'Test Post', content: 'This is a test post content') }
+
   it 'can create a post without a new tag' do
     visit 'posts/new'
     within(all('.field').first) do
-      fill_in('Name', :with => @post1.name)
-      fill_in('Content', :with => @post1.content)
+      fill_in('Name', :with => post1.name)
+      fill_in('Content', :with => post1.content)
     end
     click_button('Create Post')
-    expect(page).to have_content(@post1.name)
+    expect(page).to have_content(post1.name)
   end
 
   it 'can create a post with a new tag' do
     visit 'posts/new'
     within(all('.field').first) do
-      fill_in('Name', :with => @post1.name)
-      fill_in('Content', :with => @post1.content)
+      fill_in('Name', :with => post1.name)
+      fill_in('Content', :with => post1.content)
     end
     within(all('.field').last) do
       fill_in('Name', :with => "witty")
