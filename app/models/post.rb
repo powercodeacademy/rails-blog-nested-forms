7d6aa790f1fee52dd5f43f3fc236e92e7b249cd4
Emily Kanarek <emilyakanarek@gmail.com>
2025-08-24 14:26:40 -0400

    Accept nested tag attributes

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :post_tags
   has_many :tags, :through => :post_tags
+  accepts_tested_attributes_for :tags 
 
   validates_presence_of :name, :content
 end
