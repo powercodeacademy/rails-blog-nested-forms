aa439364e6081bcf0c0dd8d8107c3acbed13cca6
Emily Kanarek <emilyakanarek@gmail.com>
2025-08-24 14:39:29 -0400

    Reject tags if blank

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :post_tags
   has_many :tags, :through => :post_tags
-  accepts_tested_attributes_for :tags 
+  accepts_nested_attributes_for :tags, reject_if: :all_blank
 
   validates_presence_of :name, :content
 end
