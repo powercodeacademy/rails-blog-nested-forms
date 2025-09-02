97f11b364c26672e7fe12ffdea9bba2f1759cb1d
Mike <mclancy96@gmail.com>
2025-09-02 14:44:39 -0400

    Add missing files and update form helper

--- spec/features/nested_form_spec.rb
+++ spec/features/nested_form_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-describe 'nested form for tag in post', :type => :feature do
+describe 'nested form for tag in post', type: :feature do
   let(:post1) { Post.new(name: 'Test Post', content: 'This is a test post content') }
 
   it 'can create a post without a new tag' do
     visit 'posts/new'
     within(all('.field').first) do
-      fill_in('Name', :with => post1.name)
-      fill_in('Content', :with => post1.content)
+      fill_in('Name', with: post1.name)
+      fill_in('Content', with: post1.content)
     end
     click_button('Create Post')
     expect(page).to have_content(post1.name)
@@ -16,13 +16,13 @@ describe 'nested form for tag in post', :type => :feature do
   it 'can create a post with a new tag' do
     visit 'posts/new'
     within(all('.field').first) do
-      fill_in('Name', :with => post1.name)
-      fill_in('Content', :with => post1.content)
+      fill_in('Name', with: post1.name)
+      fill_in('Content', with: post1.content)
     end
     within(all('.field').last) do
-      fill_in('Name', :with => "witty")
+      fill_in('Name', with: 'witty')
     end
     click_button('Create Post')
-    expect(page).to have_content("witty")
+    expect(page).to have_content('witty')
   end
 end
